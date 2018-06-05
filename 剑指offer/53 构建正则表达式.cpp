14140022a2152445d00f7e2ed7cbf0d97617ed8c
Eyeeco <1911362549@qq.com>
2018-06-05 20:40:49 +0800

    53

---
+++ 剑指offer/53 构建正则表达式.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool match(char* str, char* pattern)
+    {
+        if(*str=='\0' && *pattern=='\0')
+            return true;
+        if(*str!='\0' && *pattern=='\0')
+            return false;
+        if(*(pattern+1)=='*'){
+            if(*str==*pattern || (*str!='\0'&&*pattern=='.'))
+                return match(str,pattern+2) || match(str+1,pattern);
+            else
+                return match(str,pattern+2);
+        }else{
+            if(*str==*pattern || (*str!='\0'&&*pattern=='.'))
+                return match(str+1,pattern+1);
+            else
+                return false;
+        }
+    }
+};
