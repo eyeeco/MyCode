aaea8a0bf62f27c6d10cc24812a5f6738831bdb4
Eyeeco <1911362549@qq.com>
2018-12-11 10:00:35 +0800

    回溯修改

--- Leetcode_Part2/39. 组合总和.cpp
+++ Leetcode_Part2/39. 组合总和.cpp
@@ -1,3 +1,4 @@
+// 自己第一遍写的回溯
 class Solution {
 public:
     void combo(vector<int>& candidates,set<vector<int>>& res,vector<int> temp, int pos, int target){
@@ -17,6 +18,26 @@ public:
         vector<int> temp;
         combo(candidates,res,temp,0,target);
         for(auto elem:res) op.push_back(elem);
+        return op;
+    }
+};
+
+// 更好的回溯
+class Solution {
+public:
+    void combo(vector<int>& candidates,vector<vector<int>>& res,vector<int> temp,int target,int pos){
+        if(target==0) {res.push_back(temp); return ;}
+        for(int i=pos;i<candidates.size()&&candidates[i]<=target;i++){
+            temp.push_back(candidates[i]);
+            combo(candidates,res,temp,target-candidates[i],i);
+            temp.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> op;
+        vector<int> temp;
+        sort(candidates.begin(),candidates.end());
+        combo(candidates,op,temp,target,0);
         return op;
     }
 };
