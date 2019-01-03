cc33674a96cb89e7f7e47472c271746820907222
Eyeeco <1911362549@qq.com>
2019-01-03 17:09:09 +0800

    二叉搜索树

---
+++ Leetcode_Part2/938. 二叉搜索树范围和.cpp
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int rangeSumBST(TreeNode* root, int L, int R) {
+        if(root==NULL) return 0;
+        if (root->val>=L && root->val<=R) {
+           return root->val+rangeSumBST(root->left,L,root->val)+rangeSumBST(root->right,root->val,R);
+        }else {
+            return rangeSumBST(root->left,L,R) + rangeSumBST(root->right,L,R);
+        }
+    }
+};
