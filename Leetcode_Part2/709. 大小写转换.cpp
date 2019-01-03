30e368fcbeab51a598dfb9646416cf6d310b5bf2
Eyeeco <1911362549@qq.com>
2019-01-03 16:51:12 +0800

    水题

---
+++ Leetcode_Part2/709. 大小写转换.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    string toLowerCase(string str) {
+        for(int i=0;i<str.size();i++){
+           if(str[i]>='A'&&str[i]<='Z') str[i]+= 32;
+        }
+        return str;
+    }
+};
