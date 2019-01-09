ebc34f7d18d3244c7de2ec6e042a2d6382d73d43
Eyeeco <1911362549@qq.com>
2019-01-09 19:36:27 +0800

    水题

---
+++ Leetcode_Part2/876. 链表中间节点.cpp
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* p1 = head;
+        ListNode* p2 = head;
+        while (p2&&p2->next) {
+            p1 = p1->next;
+            p2 = p2->next->next;
+        }
+        return p1;
+    }
+};
