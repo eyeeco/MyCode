c10f8e67e81eb2de67f948361da8413751c40113
Eyeeco <1911362549@qq.com>
2019-01-14 20:34:30 +0800

    水塘抽样（蓄水池算法）

---
+++ Leetcode_Part2/382. 链表随机节点.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    /** @param head The linked list's head.
+        Note that the head is guaranteed to be not null, so it contains at least one node. */
+    Solution(ListNode* head) {
+        this->head = head;
+    }
+
+    /** Returns a random node's value. */
+    int getRandom() {
+        ListNode* p = this->head->next;
+        int i = 2;
+        int res = this->head->val;
+        while(p) {
+            int j = rand()%i;
+            if(j==0) res = p->val;
+            i++;
+            p = p->next;
+        }
+        return res;
+    }
+    ListNode* head;
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(head);
+ * int param_1 = obj.getRandom();
+ */
